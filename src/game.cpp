149dd77566033bbd43ff5f351e86c0c332dcd945
feusx <feusuaigax@proton.me>
2025-09-27 18:42:37 +0300

    slowed down pacman but still no ghosts

--- src/game.cpp
+++ src/game.cpp
@@ -5,42 +5,55 @@ Game::Game() : pacman(1, 1), running(true){}
 
 void Game::run()
 {
+  int dx = 0, dy = 0;
+  int last_ch = ERR;
+
   while(running)
   {
     clear();
     map.draw();
-
     mvaddch(pacman.getY(), pacman.getX(), 'C');
-
     refresh();
 
     int ch = getch();
-    int dx = 0, dy = 0;
 
-    switch(ch)
+    if(ch != ERR)
     {
-      case 'w' : dy = -1; break;
-      case 's' : dy = 1; break;
-      case 'a' : dx = -1; break;
-      case 'd' : dx = 1; break;
-      default: continue;
-    }
-  
-    int new_x = pacman.getX() + dx;
-    int new_y = pacman.getY() + dy;
-
-    if(new_y >= 0 && new_y < map.getHeight() && new_x >= 0 && new_x < map.getWidth())
-    {
-      char target_char = mvinch(new_y, new_x) & A_CHARTEXT;
-
-      if(target_char != '#')
+      if(ch != last_ch)
       {
-        pacman.move(dx, dy);
+        switch(ch)
+        {
+          case 'w' : dy = -1; dx = 0; break;
+          case 's' : dy = 1; dx = 0; break;
+          case 'a' : dx = -1; dy = 0; break;
+          case 'd' : dx = 1; dy = 0; break;
+          default: dx = dy = 0; break;
+        }
 
-        if(target_char = '.')
-          map.layout[new_y][new_x] = ' ';
-          mvaddch(new_y, new_x, ' ');
+        last_ch = ch;
+
+        int new_x = pacman.getX() + dx;
+        int new_y = pacman.getY() + dy;
+
+        if(new_y >= 0 && new_y < map.getHeight() && new_x >= 0 && new_x < map.getWidth())
+        {
+          char target_char = mvinch(new_y, new_x) & A_CHARTEXT;
+          if(target_char != '#')
+          {
+            pacman.move(dx, dy);
+
+            if(target_char == '.')
+            {
+              map.layout[new_y][new_x] = ' ';
+              mvaddch(new_y, new_x, ' ');
+            }
+          }
+        }   
       }
-    }
+    }		
+		else
+		{
+		  last_ch = ERR;
+		}
   }
 } 
